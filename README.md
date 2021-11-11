69306b96072f5ef07ce98393629497e4d1ca726e
Pablo <pablobustos609@gmail.com>
2021-11-11 17:23:57 -0300

    GET routes 'ready' without models

--- README.md
+++ README.md
@@ -145,14 +145,14 @@ Se debe desarrollar un servidor en Node/Express con las siguientes rutas:
 
 __IMPORTANTE__: No está permitido utilizar los filtrados, ordenamientos y paginados brindados por la API externa, todas estas funcionalidades tienen que implementarlas ustedes.
 
-- [ ] __GET /pokemons__:
+- [x] __GET /pokemons__:
   - Obtener un listado de los pokemons desde pokeapi.
   - Debe devolver solo los datos necesarios para la ruta principal
-- [ ] __GET /pokemons/{idPokemon}__:
+- [x] __GET /pokemons/{idPokemon}__:
   - Obtener el detalle de un pokemon en particular
   - Debe traer solo los datos pedidos en la ruta de detalle de pokemon
   - Tener en cuenta que tiene que funcionar tanto para un id de un pokemon existente en pokeapi o uno creado por ustedes
-- [ ] __GET /pokemons?name="..."__:
+- [x] __GET /pokemons?name="..."__:
   - Obtener el pokemon que coincida exactamente con el nombre pasado como query parameter (Puede ser de pokeapi o creado por nosotros)
   - Si no existe ningún pokemon mostrar un mensaje adecuado
 - [ ] __POST /pokemons__:
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,87 @@
 const { Router } = require('express');
+const axios = require("axios");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.get("/pokemons", (req, res) => {
+router.get("/", (req, res) => {
+    res.json({message: "good"})
+})
 
+async function getPokemon(pokemonDB){
+    return{
+        name: pokemonDB.name,
+        hp: pokemonDB.stats[0].base_stat,
+        atk: pokemonDB.stats[1].base_stat,
+        def: pokemonDB.stats[2].base_stat,
+        speed: pokemonDB.stats[5].base_stat,
+        height: pokemonDB.height,
+        weight: pokemonDB.weight
+    }
+}
+
+async function getPokemons(pokemonsDB){
+    const pokemons = [];
+
+    for(const pokemon of pokemonsDB){
+        const info = await axios(pokemon.url);
+
+        const obj = await getPokemon(info.data);
+        
+        pokemons.push(obj);
+
+        console.log(pokemons.length)
+    }
+
+    return pokemons;
+}
+
+router.get("/pokemons", async(req, res) => {  
+    let {name} = req.query;
+
+    let result = {};
+    let pokemons = {}
+    let pokemon = {};
+
+    try{
+        if(!name){
+            // result = await axios("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1200");
+            result = await axios("https://pokeapi.co/api/v2/pokemon?offset=0&limit=15");
+    
+            pokemons = await getPokemons(result.data.results);
+    
+            res.status(200).json(pokemons);
+        }else{
+            result = await axios(`https://pokeapi.co/api/v2/pokemon/${name}`);
+
+            pokemon = await getPokemon(result.data);
+
+            console.log(pokemon)
+
+            res.status(200).json(pokemon);
+        }
+    }catch(e){
+        res.status(404).json(pokemon)
+    }
+})
+
+router.get("/pokemons/:idPokemon", async(req, res) => {     
+    try{
+        let { idPokemon } = req.params;
+
+        const result = await axios(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`);
+
+        const pokemon = await getPokemon(result.data);
+
+        console.log(pokemon)
+
+        res.status(200).json(pokemon);
+    }catch(e){
+        res.status(404).json({message: "F"});
+    }
 })
 
 
